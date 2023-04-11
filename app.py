9b1faca60fa9af84295c15c321f657d1b9ade9a1
Rodrigo Assis <rodrigomosquitu@gmail.com>
2023-04-11 20:24:25 -0300

    Versão final grupo 8

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - Teste"
+    return "Laboratório Pipeline DevOps - Versão final"
 
 if __name__ == '__main__':
     app.run()
