58c8c3bd63d50cb71934082fa95b68bb5edada0d
Rodrigo Assis <rodrigomosquitu@gmail.com>
2023-04-11 20:02:18 -0300

    Teste

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Laboratório Pipeline DevOps - Teste"
 
 if __name__ == '__main__':
     app.run()
